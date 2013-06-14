37afb042eeb4949aeeeb26b8143198738e2c44cd
Robert Thurnher <r.thurnher@gmail.com>
2013-06-14 20:37:14 +0200

    Some basic resource testing

--- src/main/java/com/example/helloworld/resources/PersonResource.java
+++ src/main/java/com/example/helloworld/resources/PersonResource.java
@@ -4,9 +4,11 @@ import com.codahale.dropwizard.hibernate.UnitOfWork;
 import com.example.helloworld.domain.Person;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import java.util.UUID;
 
+@Path("/people/{personId}")
 public interface PersonResource {
 
     @GET
