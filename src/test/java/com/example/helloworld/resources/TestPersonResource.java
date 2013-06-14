6a876f255f1c2cdbfec79dd713e788d4da8a2eda
Robert Thurnher <r.thurnher@gmail.com>
2013-06-14 20:57:25 +0200

    Enhance resource testing (JSON)

--- src/test/java/com/example/helloworld/resources/TestPersonResource.java
+++ src/test/java/com/example/helloworld/resources/TestPersonResource.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.UUID;
 import java.util.logging.Logger;
 
+import static com.codahale.dropwizard.testing.FixtureHelpers.fixture;
 import static java.util.TimeZone.getTimeZone;
 import static java.util.logging.Level.OFF;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -24,6 +25,7 @@ public class TestPersonResource extends ResourceTest {
     }
 
     private static final String PERSON_ID = "9b89ec69-e844-4e88-ada6-5ee93e947a16";
+    private static final String PERSON_PATH = "/people/" + PERSON_ID;
 
     private final Person person = createTestPerson();
 
@@ -33,7 +35,7 @@ public class TestPersonResource extends ResourceTest {
         person.setFirstName("John");
         person.setLastName("Doe");
         person.setCreatedAt(new DateTime(1371208810893L).withZone(UTC));
-        person.setBirthday(DateTime.parse("1970-1-1").withZone(UTC).toDate());
+        person.setBirthday(DateTime.parse("1980-1-1").withZone(UTC).toDate());
         person.setTimeZonePreference(getTimeZone(UTC.getID()));
         return person;
     }
@@ -52,9 +54,19 @@ public class TestPersonResource extends ResourceTest {
 
     @Test
     public void getsPersonById() throws Exception {
-        assertThat(client().resource("/people/" + PERSON_ID)
+        assertThat(client().resource(PERSON_PATH)
                 .accept(APPLICATION_JSON)
                 .get(Person.class))
                 .isEqualTo(person);
+    }
+
+    @Test
+    public void getsCorrectJson() throws Exception {
+        final String expectedJson = fixture("fixtures/person.json").replaceAll("\\s", "");
+
+        assertThat(client().resource(PERSON_PATH)
+                .accept(APPLICATION_JSON)
+                .get(String.class))
+                .isEqualTo(expectedJson);
     }
 }
