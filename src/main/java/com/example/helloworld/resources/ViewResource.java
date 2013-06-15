31dfee6878dfb24a2d1d4301de79fcfd1c8a2411
Robert Thurnher <r.thurnher@gmail.com>
2013-06-15 14:33:29 +0200

    Cleanup: annotation order :)

--- src/main/java/com/example/helloworld/resources/ViewResource.java
+++ src/main/java/com/example/helloworld/resources/ViewResource.java
@@ -10,22 +10,22 @@ import javax.ws.rs.Produces;
 public interface ViewResource {
 
     @GET
-    @Produces("text/html;charset=UTF-8")
     @Path("/utf8.ftl")
+    @Produces("text/html;charset=UTF-8")
     View freemarkerUtf8();
 
     @GET
-    @Produces("text/html;charset=ISO-8859-1")
     @Path("/iso88591.ftl")
+    @Produces("text/html;charset=ISO-8859-1")
     View freemarkerIso88591();
 
     @GET
-    @Produces("text/html;charset=UTF-8")
     @Path("/utf8.mustache")
+    @Produces("text/html;charset=UTF-8")
     View mustacheUtf8();
 
     @GET
-    @Produces("text/html;charset=ISO-8859-1")
     @Path("/iso88591.mustache")
+    @Produces("text/html;charset=ISO-8859-1")
     View mustacheIso88591();
 }
